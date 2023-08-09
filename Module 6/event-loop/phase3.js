b2f70d4898fd71d5978c4e73c857769e9979c141
thebaria04 <shalinithebariaa@gmail.com>
2023-08-10 02:17:28 +0530

    Added Phase3 code

--- Module 6/event-loop/phase3.js
+++ Module 6/event-loop/phase3.js
@@ -8,10 +8,8 @@ app.get('/', (request, response) => {
     console.log("Hello world");
 });
 app.listen(port , () => {
-    console.log("server is listening to the port `{$port}`")
+    console.log(`server is listening to the port ${port}`)
 });
-
-
 
 console.log("Start");
 
@@ -19,8 +17,18 @@ for(let i=0; i<100; ++i){
     console.log("Inside for loop");
 }
 
+// microtask
 process.nextTick(function(){
     console.log("Inside the next Tick which is micro task");
-})
+    let current = Date.now();
+    // 100 is in ms 
+    while(Date.now() - current < 1000){
 
+    } 
+})
+const timeOutScheduled = Date.now();
+setTimeout(function(){
+    const delay = Date.now() - timeOutScheduled;
+    console.log(`Inside the set timeout function and is executed with the delay ${delay}`);
+});
 console.log("End");
